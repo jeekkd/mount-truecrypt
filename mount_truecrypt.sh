4fcf5ffd0b1b10d6f66a672a2ffac8506d9706ae
huuteml <daulton@protonmail.com>
2016-05-29 01:36:11 -0500

    Updated calling declaring bash in a safer way to work across many systems, implemented better practice with variable expension and being mindful of back slashes from read, as well as control-c catch

--- mount_truecrypt.sh
+++ mount_truecrypt.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# Written by: https://github.com/turkgrb
+# Written by: https://gitlab.com/u/huuteml
 # Website: https://daulton.ca
 # Purpose: Mount truecrypt volumes
 # Notes: Run as root as privleged commands such as mount are used
@@ -16,32 +16,49 @@ crypt_name=
 media_path=/media/$crypt_name/
 #############################
 
+control_c() {
+	echo "Control-c pressed - Unmounting and closing volume. Good-bye!"
+	umount -fv $media_path/
+	cryptsetup close $crypt_name
+	exit $?
+}
+
+trap control_c SIGINT
+
 echo "Opening truecrypt container"
-cryptsetup open --type tcrypt $path_to_container $crypt_name
+cryptsetup open --type tcrypt "$path_to_container" "$crypt_name"
 if [ $? -gt 0 ]; then
 	echo "ERROR: Failed to open truecrypt container, exiting."
 	exit					
 fi
 
 echo "Mounting truecrypt container to $media_path"
-mount -w /dev/mapper/$crypt_name $media_path
+mount -w /dev/mapper/"$crypt_name" "$media_path"
 
 if [ $? -gt 0 ]; then
 	echo "$media_path must not have previously existed, creating now and retrying."
-	mkdir $media_path
-	mount -w /dev/mapper/$crypt_name $media_path					
+	mkdir "$media_path"
+	mount -w /dev/mapper/"$crypt_name" "$media_path"				
 fi
 
 echo
-echo -n "Are you ready to close the volume? Press Y, when you're done."
-read option
-case "$option" in
-	[Yy])
-		umount $media_path
-		cryptsetup close $crypt_name
-	;;
-	*)
-		echo "Make a valid selection from the menu, press Y when you're done."
-	;;
-esac
-
+for (( ; ; )); do
+	echo
+	echo "Are you ready to close the volume? Press Y when you're done."
+	read -r option
+	case "$option" in
+		[Yy])
+			echo "Unmounting and closing volume. Good-bye!"
+			umount -fv "$media_path/"
+			cryptsetup close "$crypt_name"
+			exit
+		;;
+		[Nn])
+			echo "No was selected, not exiting yet."
+			break
+		;;
+		*)
+			echo "Make a valid selection from the menu, use Y/N to select Yes or No"
+		;;
+	esac
+done
